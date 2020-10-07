1cc71d7469ff44828c855794b7dd1845c1446e36
kavilivishnu <kavilivishnu@gmail.com>
2020-10-07 22:07:03 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Children from './Children';
+
+function App() {
+  return (
+    <div className="App">
+      <Children /> 
+    </div>
+  );
+}
+
+export default App;
